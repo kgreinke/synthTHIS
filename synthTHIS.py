d2967ab065c865fba0f3c832eab7241bd8ebb564
kgreinke <160787446+kgreinke@users.noreply.github.com>
2024-02-21 12:59:07 -0800

    first commit

---
+++ synthTHIS.py
@@ -0,0 +1,7 @@
+# ***************************************************************************
+#   * Program:  synthTHIS.py
+#   * Purpose:  Synthesizer implementation in python
+#   * Author:   Kat Greinke
+#   * Date:     Today
+#   * Class:    CS410P Music, Sound, and Computers
+# ***************************************************************************
