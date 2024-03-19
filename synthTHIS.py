dfabaabe13bf5b39a5736e2731d5c082993253be
hellka7 <kgreinke@pdx.edu>
2024-03-18 19:57:48 -0700

    still tryin to do it this way

--- synthTHIS.py
+++ synthTHIS.py
@@ -9,10 +9,8 @@
 import numpy as np
 import sounddevice as sd
 import soundfile as sf
-import pygame.midi as midi
-import time
 from scipy.io import wavfile
-from scipy import signal
+import mido
 
 # -- START - "globals" --
 
@@ -59,24 +57,7 @@ midi_dev = None
 
 
 # -- midi startup --
-midi.init()
-default_id = midi.get_default_input_id()
-midi_input = midi.Input(device_id = default_id)
-count = midi.get_count()
-if log:
-    print(f"{count} midi devices found")
-for i in range(count):
-    midi_dev = midi.get_device_info(i)
-    if log:    
-        print("%s name: %s input: %s opened: %s" % (midi_dev))
-    if midi_name in midi_dev[1].decode() and midi_dev[2]:
-        device = i
-if device:
-    if log:
-        print(f"Setting device to {device}")
-    midi_dev = midi.Input(device)
-else:
-    print("Error: midi device undefined!")
+keyboard = mido.open_input()
 
 
 # *********************************************************************************************
@@ -109,6 +90,27 @@ def square_wave(frequency, time):
     wave = np.sign((frequency * time) % 2.0 - 1.0)
     return wave
 
+
+# *********************************************************************************************
+# Purpose:      Converts a midi key into a frequency 
+# Parameters:   key         -> holds an integer representation of the midi key
+# Returns:      {float}     -> float representation of the frequency of the key passed in
+# *********************************************************************************************
+def key_to_frequency(key):
+    return 440 * 2**((key - 69) / 12)
+
+
+# *********************************************************************************************
+# Purpose:      Generates a numpy linespace given a frame count 
+# Parameters:   frames          -> hold the number of frames in to generate in a sample
+# Returns:      {np.linespace}  -> object holding an array of sample times starting at clock
+# *********************************************************************************************
+def linespace_generator(frames):
+    line = np.linspace(sample_clock / sample_rate,
+                       (sample_clock + frames) / sample_rate,
+                       frames,
+                       dtype=np.float32)
+    return line
 
 # **********************************************************************************************
 # Simple record function that allows a user to record audio into a wav file
@@ -154,10 +156,124 @@ class Note:
         self.attack_remaining = attack
         self.release_remaining = None
 
+    # ******************************************************************************************
+    # Purpose:          Set note's release time remaining
+    # Parameters:       self        -> object being queried
+    # ******************************************************************************************
     def release(self):
         self.release_remaining = release
 
+    # ******************************************************************************************
+    # Purpose:          Sample generator for note object to create and shape a wave
+    # Parameters:       self        -> object being played
+    #                   linespace   -> object holding the linespace data for the note
+    # Return:           samples     -> numpy array with a wave to be played
+    # ******************************************************************************************
     def samples(self, linespace):
-        global out_keys
+        if self.playing == False:
+            return None
+        
+        freq = self.frequency
+        count = len(linespace)
 
-        for midi_in in midi.Input.read(self)
+        #determine oscillator type currently in use
+        if osc_type == 'sine':
+            samples = sine_wave(freq, linespace)
+        elif osc_type == 'square':
+            samples = square_wave(freq, linespace)
+        else:
+            samples = saw_wave(freq, linespace)
+
+        #calculate release 
+        if self.release_remaining is not None:
+            release_copy = self.release_remaining
+            if release_copy <= 0:
+                self.playing = False
+                return None
+            
+            start_gain = release_copy / release
+            end = count / sample_rate
+            release_copy -= end
+            end_gain = release_copy / release
+
+            envelope = np.clip(np.linspace(start_gain, end_gain, count), 0.0, 1.0)
+
+            samples *= envelope
+
+            self.release_remaining = max(0, release_copy)
+
+        #calculate attack
+        elif self.attack_remaining > 0.0:
+            attack_copy = self.attack_remaining
+            if attack_copy <= 0:
+                self.playing = False
+                return None
+            
+            start_gain = 1.0 - attack_copy / attack
+            end = count / sample_rate
+            attack_copy -= end
+            end_gain = 1.0 - attack_copy / attack
+
+            envelope = np.clip(np.linspace(start_gain, end_gain, count), 0.0, 1.0)
+
+            samples *= envelope
+
+            self.attack_remaining = max(0, attack_copy)
+
+        return samples
+
+#               - - End Of Note Class --                #
+
+# ******************************************************************************************
+# Purpose:          Funtion called by sounddevice to get some samples to output.    
+# Parameters:       data        -> samples to be made into sound
+#                   count       -> integer representation of the frame count
+#                   status      -> status of the callback
+# ******************************************************************************************
+def output_callback(data, count, status):
+    global sample_clock
+
+    if status:
+        print("output callback:", status)
+
+    samples = np.zeros(count, dtype=np.float32)
+
+    if out_keys:
+        line = linespace_generator(count)
+
+        on = list(out_keys.keys())
+        off = set()
+
+        for key in on:
+            note = out_keys[key]
+            note_samples = note.samples(line)
+            if note_samples is None:
+                off.add(key)
+                continue
+            samples += note_samples
+
+        for key in off:
+            del out_keys[key]
+
+    keys = len(out_keys)
+    if keys <= 8:
+        samples *= 1.0 / 8.0
+    else:
+        samples *= 1.0 / len(out_keys)
+
+    #reshape samples
+    data[:] = np.reshape(samples, (count, 1))
+
+    sample_clock += count
+
+
+# ******************************************************************************************
+# Purpose:          Process MIDI events accordingly    
+# Parameters:       data        -> samples to be made into sound
+#                   count       -> integer representation of the frame count
+#                   status      -> status of the callback
+# ******************************************************************************************
+def process_midi_event():
+    global out_keys, osc_type
+
+    message = keyboard.recieve()
